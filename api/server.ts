79b4105f2a59f48bc61bb8a3caf09b6673ecf91b
ablecrew <teddy.dande4@gmail.com>
2026-03-28 17:05:54 +0300

    bind to 0.0.0.0 for render

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Dev server running on http://localhost:${PORT}`);
 });
