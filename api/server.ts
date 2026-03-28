dbf3c1c96a0a72d53176403987d02584449d4080
ablecrew <teddy.dande4@gmail.com>
2026-03-28 17:15:40 +0300

    fix PORT type for render

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Dev server running on http://localhost:${PORT}`);
 });
